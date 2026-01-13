b1c9e3817e9567b3757a1c0bc0a5fa850dd83223
abimehta777 <abhyanandkumar997@gmail.com>
2026-01-13 23:39:52 +0530

    save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,19 @@
- export let cart = [{
+ export let cart = JSON.parse (localStorage.getItem('cart'));
+
+ if(!cart){
+  cart = [{
   productsId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 2,
  },{
    productsId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
   quantity: 1
  }];
+ }
+
+ 
+ function saveToStorage(){
+  localStorage.setItem('cart', JSON.stringify(cart));
+ }
           
 export function addToCart (productsId){
 
@@ -24,6 +33,8 @@ let MatchingItem;
       quantity: 1
     });
   }
+
+  saveToStorage();
 }
 
 export function removeFromCart(productsId){
@@ -36,4 +47,6 @@ export function removeFromCart(productsId){
    });
 
    cart = newCart;
+   saveToStorage();
+
 };
